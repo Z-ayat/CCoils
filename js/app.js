3433b1ad5ac1170b181850fab518d2aa68cbeb5f
AliEl-Zayat <alielzayat07@gmail.com>
2022-08-02 19:32:01 +0200

    Product List
    -fixed everything with slider
    -added products stuck on giving gaps with bootstrap

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
     spaceBetween: 30,
-    centeredSlides: true,
+    loop: true,
     autoplay: {
         delay: 2000,
         disableOnInteraction: false,
